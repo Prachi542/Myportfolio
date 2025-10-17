3813bb3a86c1b0b2bf3db8e866e41c5ee14f15cc
Prachi Srivastava <mailtoprachigithub13@gmail.com>
2025-10-17 20:04:06 +0530

    first

---
+++ day1/scripts.js
@@ -0,0 +1,10 @@
+documents.getElementById("college-name")
+
+function hello(){
+    //documents.getElementaryById("Welcome-btn").onclick=hello();
+    document.getElementById(welcome-btn).addEventListener("click",()=>{
+alert("Button Clicked FINALLYYY!!!")
+Document.getElementById("")
+
+
+
